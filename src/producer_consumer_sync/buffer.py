b157ff93d18b1bbb494d75679ed206e1bc1a2b26
Guilherme Carvalho <dev.gcarvalho@gmail.com>
2025-02-09 20:00:48 +0000

    feat: cria classe abstrata Buffer com contrato

--- src/producer_consumer_sync/buffer.py
+++ src/producer_consumer_sync/buffer.py
@@ -1,2 +1,11 @@
-class Buffer():
-    pass
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Buffer(ABC):
+    @abstractmethod
+    def insert(self, data: Any) -> None:
+        pass
+
+    @abstractmethod
+    def remove(self) -> Any:
+        pass
