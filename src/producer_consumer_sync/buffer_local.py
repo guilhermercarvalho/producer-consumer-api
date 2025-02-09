9fbb44c9b2fa2313ff7c42185eef584428761bd1
Guilherme Carvalho <dev.gcarvalho@gmail.com>
2025-02-09 19:10:30 +0000

    test fix: configura propriedades estáticas

--- src/producer_consumer_sync/buffer_local.py
+++ src/producer_consumer_sync/buffer_local.py
@@ -1,39 +1,63 @@
-from tests import Buffer
+from src.producer_consumer_sync.buffer import Buffer
 
-class BufferLocal(Buffer):
+
+class BufferLocal():
     def __init__(self, size):
         super().__setattr__("_size", size)
-        self.slots = [None] * size
-        self.pointer = 0
+        super().__setattr__("_slots", [None] * size)
+        super().__setattr__("_pointer", 0)
 
-        self.invalid_access = 0
-        self.skipped_insert = 0
+        super().__setattr__("_invalid_access", 0)
+        super().__setattr__("_skipped_insert", 0)
 
     def insert(self, data):
-        hasAnyEmptySlot = self.pointer < self._size
+        hasAnyEmptySlot = self.pointer < self.size
 
         if hasAnyEmptySlot:
             self.slots[self.pointer] = data
-            self.pointer += 1
+            self._pointer += 1
         else:
-            self.skipped_insert += 1
+            self._skipped_insert += 1
 
     def remove(self):
         hasAnySlotFulfilled = self.pointer > 0
 
         if hasAnySlotFulfilled:
             data = self.slots[self.pointer - 1]
-            self.pointer -= 1
+            self._pointer -= 1
             return data
         else:
-            self.invalid_access += 1
+            self._invalid_access += 1
             return -1
 
     @property
     def size(self):
         return self._size
 
+    @property
+    def slots(self):
+        return self._slots
+
+    @property
+    def pointer(self):
+        return self._pointer
+
+    @property
+    def invalid_access(self):
+        return self._invalid_access
+
+    @property
+    def skipped_insert(self):
+        return self._skipped_insert
+
     def __setattr__(self, name, value):
-        if name == "_size":
-            raise AttributeError("Attribute '_size' cannot be modified!")
+        static_values = [
+            "_size",
+            "_slots",
+            # "_pointer",
+            # "_invalid_access",
+            # "_skipped_insert"
+        ]
+        if name in static_values:
+            raise AttributeError(f"Attribute '{name}' cannot be modified!")
         super().__setattr__(name, value)
