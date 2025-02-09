5c7248f62e4dd1892ede686d9f47b942470caee2
Guilherme Carvalho <dev.gcarvalho@gmail.com>
2025-02-09 20:01:46 +0000

    test refactor: adiciona fixtures para organização dos testes

--- tests/test_buffer_local.py
+++ tests/test_buffer_local.py
@@ -3,57 +3,84 @@ import pytest
 from src.producer_consumer_sync.buffer_local import BufferLocal
 
 
+@pytest.fixture
+def create_buffer(request):
+    # Obtém o valor do parâmetro "size" ou usa o valor padrão 10
+    size = getattr(request, "param", 10)
+    return BufferLocal(size)
+
+@pytest.fixture
+def buffer_with_slot_fulfilled(request, create_buffer):
+    data = getattr(request, "param", 1)
+    create_buffer.insert(data)
+    return create_buffer
+
 class TestBufferLocal:
-    @pytest.mark.parametrize("size", [1, 5, 10])
-    def test_create(self, size):
-        buffer = BufferLocal(size)
+    @pytest.mark.parametrize("create_buffer", [1, 5, 10], indirect=True)
+    def test_create(self, create_buffer):
+        buffer = create_buffer
 
         assert buffer.pointer == 0
-        assert buffer.size == size
-        assert len(buffer.slots) == size
+        assert buffer.size in [1, 5, 10]
+        assert len(buffer.slots) in [1, 5, 10]
 
-    # TODO: Utilizar parametros para testar alterar os atributos privados
-    def test_change_default_size(self):
-        buffer = BufferLocal(10)
+    # ! testar alterar os atributos privados
+    def test_change_default_size(self, create_buffer):
+        buffer = create_buffer
 
         with pytest.raises(AttributeError) as err:
             buffer._size = 1
 
         assert str(err.value) == "Attribute '_size' cannot be modified!"
 
-    # TODO: Simular por parâmetro até capacidade máxima do buffer
-    def test_insert_data_in(self):
-        buffer = BufferLocal(10)
-        buffer.insert(123)
+    def test_insert(self, buffer_with_slot_fulfilled):
+        buffer = buffer_with_slot_fulfilled
 
-        assert buffer.slots[0] == 123
+        assert buffer.slots[0] == 1
         assert buffer.pointer == 1
 
-    @pytest.mark.parametrize("value", [1, 2, 3, 4, 5])
-    def test_inserts_data_in(self, value):
-        buffer = BufferLocal(5)
-        buffer.insert(value)
+    def test_insert_until_max_capacity(self, create_buffer):
+        buffer = create_buffer
 
-        assert buffer.slots[0] == value
-        assert buffer.pointer == 1
+        for i in range(10):
+            buffer.insert(i)
 
-    def test_insert_data_in_fulfilled(self):
-        buffer = BufferLocal(1)
-        buffer.insert(123)
-        buffer.insert(456)
+        assert buffer.slots == list(range(10))
+        assert buffer.pointer == 10
 
-        assert buffer.remove() != 456
+    @pytest.mark.parametrize("value", [1, 5, 10])
+    def test_insert_changing_capacity(self, create_buffer, value):
+        buffer = create_buffer
+
+        range_values = list(range(value))
+
+        for i in range_values:
+            buffer.insert(i)
+
+        assert buffer.slots[:len(range_values)] == range_values
+        assert buffer.pointer == value
+
+    @pytest.mark.parametrize("create_buffer", [1], indirect=True)
+    def test_insert_above_maximum_capacity(self, create_buffer):
+        buffer = create_buffer
+
+        values = [0, 1]
+        last_value = values[len(values) - 1]
+
+        for value in values:
+            buffer.insert(value)
+
+        assert buffer.remove() != last_value
         assert buffer.skipped_insert == 1
 
-    def test_remove_data_in(self):
-        buffer = BufferLocal(10)
-        buffer.insert(123)
+    def test_remove(self, buffer_with_slot_fulfilled):
+        buffer = buffer_with_slot_fulfilled
 
-        assert buffer.remove() == 123
+        assert buffer.remove() == 1
         assert buffer.pointer == 0
 
-    def test_remove_data_in_empty(self):
-        buffer = BufferLocal(10)
+    def test_remove_when_empty(self, create_buffer):
+        buffer = create_buffer
 
         assert buffer.remove() == -1
         assert buffer.invalid_access == 1
--- tests/test_buffer_with_semaphore.py
+++ tests/test_buffer_with_semaphore.py
@@ -3,16 +3,30 @@ from threading import Thread
 
 from src.producer_consumer_sync.buffer_with_semaphore import BufferWithSemaphore
 
+
+@pytest.fixture
+def create_buffer(request):
+    # Obtém o valor do parâmetro "size" ou usa o valor padrão 10
+    size = getattr(request, "param", 10)
+    return BufferWithSemaphore(size)
+
+
+@pytest.fixture
+def buffer_with_slot_fulfilled(request, create_buffer):
+    data = getattr(request, "param", 1)
+    create_buffer.insert(data)
+    return create_buffer
+
+
 class TestBufferWithSemaphore:
-    @pytest.mark.parametrize("size", [1, 5, 10])
-    def test_create(self, size):
-        buffer = BufferWithSemaphore(size)
+    @pytest.mark.parametrize("create_buffer", [1, 5, 10], indirect=True)
+    def test_create(self, create_buffer):
+        buffer = create_buffer
 
         assert buffer.pointer == 0
-        assert buffer.size == size
-        assert len(buffer.slots) == size
+        assert buffer.size in [1, 5, 10]
+        assert len(buffer.slots) in [1, 5, 10]
 
-    # TODO: Utilizar parametros para testar alterar os atributos privados
     def test_change_default_size(self):
         buffer = BufferWithSemaphore(10)
 
@@ -21,31 +35,42 @@ class TestBufferWithSemaphore:
 
         assert str(err.value) == "Attribute '_size' cannot be modified!"
 
-    # TODO: Simular por parâmetro até capacidade máxima do buffer
-    def test_insert_data(self):
-        buffer = BufferWithSemaphore(10)
-        buffer.insert(123)
+    def test_insert(self, buffer_with_slot_fulfilled):
+        buffer = buffer_with_slot_fulfilled
 
-        assert buffer.slots[0] == 123
+        assert buffer.slots[0] == 1
         assert buffer.pointer == 1
 
-    @pytest.mark.parametrize("value", [1, 2, 3, 4, 5])
-    def test_insert_until_max_capacity(self, value):
-        buffer = BufferWithSemaphore(5)
-        buffer.insert(value)
+    def test_insert_until_max_capacity(self, create_buffer):
+        buffer = create_buffer
 
-        assert buffer.slots[0] == value
-        assert buffer.pointer == 1
+        for i in range(10):
+            buffer.insert(i)
 
-    def test_insert_data_in_fulfilled(self):
-        buffer = BufferWithSemaphore(1)
-        buffer.insert(123)
+        assert buffer.slots == list(range(10))
+        assert buffer.pointer == 10
+
+    @pytest.mark.parametrize("value", [1, 5, 10])
+    def test_insert_changing_capacity(self, create_buffer, value):
+        buffer = create_buffer
+
+        range_values = list(range(value))
+
+        for i in range_values:
+            buffer.insert(i)
+
+        assert buffer.slots[:len(range_values)] == range_values
+        assert buffer.pointer == value
+
+    @pytest.mark.parametrize("create_buffer", [1], indirect=True)
+    def test_insert_above_maximum_capacity(self, create_buffer):
+        buffer = create_buffer
+        buffer.insert(1)
 
         def insert():
-            buffer.insert(456)
+            buffer.insert(2)
 
         thread = Thread(target=insert)
-
         thread.start()
 
         import time
@@ -55,15 +80,14 @@ class TestBufferWithSemaphore:
         assert buffer.pointer == 1
         assert buffer.skipped_insert == 0
 
-    def test_remove_data_in(self):
-        buffer = BufferWithSemaphore(10)
-        buffer.insert(123)
+    def test_remove(self, buffer_with_slot_fulfilled):
+        buffer = buffer_with_slot_fulfilled
 
-        assert buffer.remove() == 123
+        assert buffer.remove() == 1
         assert buffer.pointer == 0
 
-    def test_remove_data_in_empty(self):
-        buffer = BufferWithSemaphore(10)
+    def test_remove_when_empty(self, create_buffer):
+        buffer = create_buffer
 
         def remove():
             buffer.remove()
